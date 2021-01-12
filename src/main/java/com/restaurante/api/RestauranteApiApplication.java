189a367213a4f41d36fd26fc42ba31d5b118d34c
Francisco de Assis <work.chico@gmail.com>
2021-01-12 17:38:17 -0300

    Estendendo o JpaRepository para customizar o repositório base

--- src/main/java/com/restaurante/api/RestauranteApiApplication.java
+++ src/main/java/com/restaurante/api/RestauranteApiApplication.java
@@ -3,8 +3,12 @@ package com.restaurante.api;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.restaurante.api.infraestructure.repositories.CustomJPARepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJPARepositoryImpl.class)
 public class RestauranteApiApplication implements CommandLineRunner{
 	
 	public static void main(String[] args) {
--- src/main/java/com/restaurante/api/domain/repositories/RestauranteRepository.java
+++ src/main/java/com/restaurante/api/domain/repositories/RestauranteRepository.java
@@ -1,11 +1,28 @@
 package com.restaurante.api.domain.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.restaurante.api.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
+public interface RestauranteRepository
+		extends CustomJPARepository<Restaurante, Long>, RestauranteRepositoryQueries,
+		JpaSpecificationExecutor<Restaurante>{
+	
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	List<Restaurante> findByTaxaFreteBetweenAndCozinhaNomeContains(BigDecimal taxaInicial, BigDecimal taxaFinal, String nome);
+	
+	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :cozinha")
+	List<Restaurante> consultaPorNome(String nome, Long cozinha);
+	
+//	List<Restaurante> find(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
 }
