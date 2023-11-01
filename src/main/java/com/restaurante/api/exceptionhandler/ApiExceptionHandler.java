7811b4f1e54f047d5abdd0ef62dc13e70a1a156b
FRANCISCO DE ASSIS DA SILVA JUNIOR <francisco.a.silva@fiesc.com.br>
2023-11-01 11:42:23 -0300

    Desafio: implementando exception handler

--- src/main/java/com/restaurante/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/restaurante/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.restaurante.api.domain.exceptions.EntidadeEmUsoException;
 import com.restaurante.api.domain.exceptions.EntidadeNaoEncontradaException;
 import com.restaurante.api.domain.exceptions.NegocioException;
 
@@ -45,6 +46,18 @@ public class ApiExceptionHandler {
 				.mensagem("O tipo de mídia não é suportado").build();
 		
 		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+				.body(problema);
+	}
+	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(
+			EntidadeEmUsoException e){
+		
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage()).build();
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT)
 				.body(problema);
 	}
 
